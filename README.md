517d66c329367517bdea29d50dd2f04d4577400e
Brody Chen <rufengch@gmail.com>
2018-05-29 22:37:14 -0700

    Add effectiveMaps for the other 6 types

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # pokemon-chooser
 
+### Reference
+
+https://egghead.io/courses/how-to-write-an-open-source-javascript-library
+
 pokemon types reference: https://pokemondb.net/type
 
 > Note: this chart is for games from 2013 onwards - Pokémon X/Y, OR/AS, Pokémon GO and Sun/Moon. Type charts for older games have some minor differences.
--- src/effectiveMap.js
+++ src/effectiveMap.js
@@ -50,4 +50,64 @@ export const effectiveMaps = {
     half: [types.ice],
     zero: [],
   },
+  [types.fighting]: {
+    double: [types.flying, types.psychic, types.fairy],
+    half: [types.bug, types.rock, types.dark],
+    zero: [],
+  },
+  [types.poison]: {
+    double: [types.ground, types.psychic],
+    half: [types.grass, types.fighting, types.poison],
+    zero: [],
+  },
+  [types.ground]: {
+    double: [types.water, types.grass, types.ice],
+    half: [types.poison, types.rock],
+    zero: [types.electric],
+  },
+  [types.flying]: {
+    double: [types.electric, types.ice, types.rock],
+    half: [types.grass, types.fighting, types.bug],
+    zero: [types.ground],
+  },
+  [types.psychic]: {
+    double: [types.bug, types.ghost, types.dark],
+    half: [types.fighting, types.psychic],
+    zero: [],
+  },
+  [types.bug]: {
+    double: [types.fire, types.flying, types.rock],
+    half: [types.grass, types.fighting, types.ground],
+    zero: [],
+  },
+  [types.rock]: {
+    double: [types.water, types.grass, types.fighting, types.ground, types.steel],
+    half: [types.normal, types.fire, types.poison, types.flying],
+    zero: [],
+  },
+  [types.ghost]: {
+    double: [types.ghost, types.dark],
+    half: [types.poison, types.bug],
+    zero: [types.normal, types.fighting],
+  },
+  [types.dragon]: {
+    double: [types.ice, types.dragon, types.fairy],
+    half: [types.fire, types.water, types.electric, types.grass],
+    zero: [],
+  },
+  [types.dark]: {
+    double: [types.fighting, types.bug, types.fairy],
+    half: [types.ghost, types.dark],
+    zero: [types.psychic],
+  },
+  [types.steel]: {
+    double: [types.fire, types.fighting, types.ground],
+    half: [types.normal, types.grass, types.ice, types.flying, types.psychic, types.bug, types.rock, types.dragon, types.steel, types.fairy],
+    zero: [types.poison],
+  },
+  [types.fairy]: {
+    double: [types.poison, types.steel],
+    half: [types.fighting, types.bug, types.dark],
+    zero: [types.dragon],
+  },
 };
