7cd29ddbf114e16a3147071ba5e105d76faf76fa
Brody Chen <rufengch@gmail.com>
2018-05-30 22:40:22 -0700

    Implement fightAgainst function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import effectiveMaps from "./effectiveMaps";
 
 const fightAgainst = (oponentType) => {
-
+  const types = effectiveMaps[oponentType];
+  if (types.double.length) return types.double;
+  if (types.half.length) return types.half;
+  return types.zero;
 };
 
 module.export = {
   fightAgainstType,
-  // fightAgainstTypes,
 };
