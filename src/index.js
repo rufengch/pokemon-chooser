2378ddbb6b3f18847b987e5ab28dfafe932ff189
Brody Chen <rufengch@gmail.com>
2018-06-01 22:03:16 -0700

    Change fightAgainst to return object

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 const effectiveMaps = require('./effectiveMap');
 
-const fightAgainst = (oponentType) => {
-  const types = effectiveMaps[oponentType];
-  if (types.double.length) return types.double;
-  if (types.half.length) return types.half;
-  return types.zero;
-};
+const fightAgainst = oponentType => effectiveMaps[oponentType];
 
 module.exports = {
   fightAgainst,
