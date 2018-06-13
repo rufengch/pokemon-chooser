b615e588808fa271a3936cd1447dd791b11d4a57
Brody Chen <rufengch@gmail.com>
2018-06-12 22:52:39 -0700

    Support single-type pokemon

--- src/index.js
+++ src/index.js
@@ -1,10 +1,61 @@
 const effectiveMaps = require('./effectiveMap');
+const generation1 = require('./pokedex/generation-1');
+const generation2 = require('./pokedex/generation-2');
+const generation3 = require('./pokedex/generation-3');
+const generation4 = require('./pokedex/generation-4');
+const generation5 = require('./pokedex/generation-5');
+const generation6 = require('./pokedex/generation-6');
+const generation7 = require('./pokedex/generation-7');
 
-const fightAgainst = oponentType => effectiveMaps[oponentType];
+const generationData = [
+  generation1,
+  generation2,
+  generation3,
+  generation4,
+  generation5,
+  generation6,
+  generation7,
+];
 
-// const getType = pokemonName => pokemonTypes[pokemonName];
+const getPokemonNameToTypeMap = () => {
+  return generationData.reduce((result, generation) => {
+    return generation.reduce((acc, obj) => {
+      acc[obj.name.toLowerCase()] = obj.types;
+      return acc;
+    }, result);
+  }, {});
+};
+
+const getResultByType = type => {
+  const typeResult = effectiveMaps[type];
+  const existingTypes = typeResult.superEffective.concat(typeResult.notVeryEffective).concat(typeResult.noEffect);
+  typeResult.normal = Object.keys(effectiveMaps).filter(type => {
+    return !existingTypes.includes(type);
+  });
+  return typeResult;
+};
+
+const fightAgainst = string => {
+  // string should be a type or a pokomon's name
+  if (!string) return {};
+  
+  string = string.toLowerCase();
+  
+  if (effectiveMaps[string]) {
+    return getResultByType(string);
+  }
+
+  const pokemonNameToTypeMap = getPokemonNameToTypeMap();
+  const pokemonTypes = pokemonNameToTypeMap[string];
+  if (pokemonTypes) {
+    if (pokemonTypes.length === 1) return getResultByType(pokemonTypes[0].toLowerCase());
+
+    // the pokemon belongs to two types
+  }
+
+  return {};
+};
 
 module.exports = {
-  // getType,
   fightAgainst,
 };
--- src/test.js
+++ src/test.js
@@ -1,6 +1,5 @@
 const pokemonChooser = require('./index');
 
-console.log(pokemonChooser.fightAgainst('fire'));
-console.log(pokemonChooser.fightAgainst('grass'));
-console.log(pokemonChooser.fightAgainst('water'));
-console.log(pokemonChooser.fightAgainst('electric'));
+// console.log(pokemonChooser.fightAgainst('fire'));
+// console.log(pokemonChooser.fightAgainst('Ivysaur'));
+console.log(pokemonChooser.fightAgainst('Litten'));
